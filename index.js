c26a9176b3efe65a49739b36ae3bbc1567bfa5b6
woobaorong <243583785@qq.com>
2024-06-24 19:11:37 +0800

    dfghdf

--- index.js
+++ index.js
@@ -13,49 +13,57 @@ app.use(cors());
 app.use(logger);
 
 // 首页
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", async(req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
+app.post("/api/count", async(req, res) => {
+    const { action } = req.body;
+    if (action === "inc") {
+        await Counter.create();
+    } else if (action === "clear") {
+        await Counter.destroy({
+            truncate: true,
+        });
+    }
+    res.send({
+        code: 0,
+        data: await Counter.count(),
     });
-  }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
 });
 
 // 获取计数
-app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
+app.get("/api/count", async(req, res) => {
+    const result = await Counter.count();
+    res.send({
+        code: 0,
+        data: result,
+    });
+});
+
+app.get("/api/xixi", async(req, res) => {
+    const result = await Counter.count();
+    res.send({
+        code: 0,
+        data: "xixi",
+    });
 });
 
 // 小程序调用，获取微信 Open ID
-app.get("/api/wx_openid", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
-  }
+app.get("/api/wx_openid", async(req, res) => {
+    if (req.headers["x-wx-source"]) {
+        res.send(req.headers["x-wx-openid"]);
+    }
 });
 
 const port = process.env.PORT || 80;
 
 async function bootstrap() {
-  await initDB();
-  app.listen(port, () => {
-    console.log("启动成功", port);
-  });
+    await initDB();
+    app.listen(port, () => {
+        console.log("启动成功", port);
+    });
 }
 
 bootstrap();
